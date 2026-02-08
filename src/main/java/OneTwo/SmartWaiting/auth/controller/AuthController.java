b16c8b75eec50b30a3cd9c03d7d86d2d0807a2a9
Kim Hani <rlahani99@gmail.com>
2026-02-08 22:42:52 +0900

    refactor/oauth : Move AuthControllerClass's package

--- src/main/java/OneTwo/SmartWaiting/auth/controller/AuthController.java
+++ src/main/java/OneTwo/SmartWaiting/auth/controller/AuthController.java
@@ -1,4 +1,4 @@
-package OneTwo.SmartWaiting.auth.dto.controller;
+package OneTwo.SmartWaiting.auth.controller;
 
 import OneTwo.SmartWaiting.auth.dto.requestDto.AdminSignUpRequestDto;
 import OneTwo.SmartWaiting.auth.dto.requestDto.SignInRequestDto;
