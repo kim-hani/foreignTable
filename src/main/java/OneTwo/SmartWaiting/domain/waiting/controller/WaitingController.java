d7ceb67884bdaccaf2056c180e972e277900d665
Kim Hani <rlahani99@gmail.com>
2026-02-24 16:59:52 +0900

    refactor/security : Apply security to store and waiting logic

--- src/main/java/OneTwo/SmartWaiting/domain/waiting/controller/WaitingController.java
+++ src/main/java/OneTwo/SmartWaiting/domain/waiting/controller/WaitingController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -22,22 +23,26 @@ public class WaitingController {
 
     // 대기 등록
     @PostMapping
-    public ResponseEntity<Long> registerWaiting(@RequestBody @Valid WaitingRegisterRequestDto request) {
-        Long waitingId = waitingService.registerWaiting(request);
+    public ResponseEntity<Long> registerWaiting(
+            Principal principal,
+            @RequestBody @Valid WaitingRegisterRequestDto request) {
+        Long waitingId = waitingService.registerWaiting(request,principal.getName());
         return ResponseEntity.created(URI.create("/api/v1/waitings/" + waitingId)).body(waitingId);
     }
 
     // 대기 취소
     @PatchMapping("/{waitingId}/cancel")
-    public ResponseEntity<Void> cancelWaiting(@PathVariable Long waitingId) {
-        waitingService.cancelWaiting(waitingId);
+    public ResponseEntity<Void> cancelWaiting(
+            @PathVariable Long waitingId,
+            Principal principal) {
+        waitingService.cancelWaiting(waitingId,principal.getName());
         return ResponseEntity.ok().build();
     }
 
     // 내 웨이팅 목록 조회
     @GetMapping("/my")
-    public ResponseEntity<List<WaitingResponse>> getMyWaitings(@RequestParam Long memberId) {
-        return ResponseEntity.ok(waitingService.getMyWaitings(memberId));
+    public ResponseEntity<List<WaitingResponse>> getMyWaitings(Principal principal) {
+        return ResponseEntity.ok(waitingService.getMyWaitings(principal.getName()));
     }
 
     // 가게 웨이팅 목록 조회
@@ -52,8 +57,9 @@ public class WaitingController {
     @PatchMapping("/{waitingId}/status")
     public ResponseEntity<Void> changeStatus(
             @PathVariable Long waitingId,
+            Principal principal,
             @RequestBody @Valid WaitingChangeRequestDto request) {
-        waitingService.changeStatus(waitingId, request);
+        waitingService.changeStatus(waitingId, request, principal.getName());
         return ResponseEntity.ok().build();
     }
 }
