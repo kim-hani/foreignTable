791a7e1b240f037fff2c27d3f6377141ff7d20ea
Kim Hani <rlahani99@gmail.com>
2026-03-09 17:14:34 +0900

    feat/paging : Apply pagination when retrieving reviews

--- src/main/java/OneTwo/SmartWaiting/domain/review/service/ReviewService.java
+++ src/main/java/OneTwo/SmartWaiting/domain/review/service/ReviewService.java
@@ -13,6 +13,7 @@ import OneTwo.SmartWaiting.domain.waiting.enums.WaitingStatus;
 import OneTwo.SmartWaiting.domain.waiting.repository.WaitingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -66,10 +67,9 @@ public class ReviewService {
     }
 
     // 2. 가게별 리뷰 조회
-    public List<ReviewResponseDto> getStoreReviews(Long storeId, Pageable pageable) {
+    public Slice<ReviewResponseDto> getStoreReviews(Long storeId, Pageable pageable) {
         return reviewRepository.findAllByStoreIdOrderByCreatedAtDesc(storeId,pageable)
                 .map(ReviewResponseDto::from);
-
     }
 
     // 3. 리뷰 삭제 (작성자 본인 확인 필요 - 일단은 ID만 받아서 삭제)
